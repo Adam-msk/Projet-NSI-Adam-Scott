64de73f1b123f9761e50ffacd0b9b3f47eec0615
Adam-msk <adam.family531@gmail.com>
2026-01-08 14:49:08 +0100

    Add files via upload

--- Projet NSI/main.py
+++ Projet NSI/main.py
@@ -8,9 +8,17 @@ pygame.display.set_caption("Flappy Bird NSI")
 
 background = pygame.image.load("images/background.png").convert()
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+start_button = pygame.image.load("images/start_button.png").convert_alpha()
+start_button = pygame.transform.scale(start_button, (200, 200))
+start_rect = start_button.get_rect(center=(150, 320))
+# start_rect.inflate_ip(-40, -40)
+button_rect = start_button.get_rect()
+button_rect.size = (200, 74)
+button_rect.center = (start_rect.centerx, start_rect.centery)
 
 clock = pygame.time.Clock()
 running = True
+game_state = "menu"
 
 while running:
     clock.tick(FPS)
@@ -19,7 +27,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.blit(background, (0, 0))
+        if game_state == "menu":
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if button_rect.collidepoint(event.pos):
+                    game_state = "game"
+
+    if game_state == "menu":
+        screen.blit(background, (0, 0))
+        screen.blit(start_button, start_rect)
+        # pygame.draw.rect(screen, (0,0,0), button_rect, 2)
+
+    elif game_state == "game":
+        screen.fill((135, 206, 235)) 
     pygame.display.flip()
 
 pygame.quit()
